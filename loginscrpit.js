b442e6ebc2ba582c397b6e23ff726955fd055f1a
Harkiran Kaur <98973889+Harkirankaur06@users.noreply.github.com>
2025-03-06 21:09:05 +0530

    Delete loginscrpit.js

--- loginscrpit.js
+++
@@ -1,9 +0,0 @@
-const conatiner = document.querySelector('.container');
-const registerBtn = document.querySelector('.register-btn');
-const loginBtn = document.querySelector('.login-btn');
-registerBtn.addEventListener('click', () => { 
-    conatiner.classList.add('active');
-});
-loginBtn.addEventListener('click', () => { 
-    conatiner.classList.remove('active');
-});
